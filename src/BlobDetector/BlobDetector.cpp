80aada81b0c29b1bdbd3596d39b1da471f6d7473
Brayden Streeter <bstreete@email.arizona.edu>
2015-10-31 12:31:23 -0700

    Removed the open/close portion of the algorithm and cleaned up the getopts loop to no longer look for the open/close arguments.

--- src/BlobDetector/BlobDetector.cpp
+++ src/BlobDetector/BlobDetector.cpp
@@ -17,26 +17,24 @@ using namespace std;
 int debug = 0;
 
 // Function prototypes
-Mat smooth_input(Mat img, int count, int size, int type);
 Mat region_filling(Mat image, int* size, int range);
 int calcArea(int radius);
 Mat threshold(Mat src);
-int check_args(int *size, int *type, int *count, int *range, std::string *file_name,
-               std::string *output_name, int argc, char** argv);
 void merge(const Mat &image, Mat &image2, Mat &result, string output_name);
+int check_args(int *range, std::string *file_name, std::string *output_name, 
+    int argc, char** argv);
 void usage(char* binary);
 
 
 /** @function main */
 int main( int argc, char** argv ){
     std::string file_name, output_name;
-    int size = 5, type = MORPH_ELLIPSE, count = 5, status, range = 10;
+    int status, range = 10, area = 0;
     Mat src, originalImage, TexCamOut, gray_scale, output, HazMap;
     std::vector<std::vector<Point>> contours;
-    int area = 0;
     
     // Check the passed arguments
-    status = check_args(&size, &type, &count, &range, &file_name, &output_name, argc, argv);
+    status = check_args(&range, &file_name, &output_name, argc, argv);
     // Usage or help requested
     if (status == -1) return 0;
     // Invalid arguments passed
@@ -45,20 +43,16 @@ int main( int argc, char** argv ){
     // Read the textureCam output image and save a copy for the super imposition
     //file_name = file_name+".ppm";
     src = imread(file_name+".ppm");
-    TexCamOut = imread(file_name+".ppm");
+    TexCamOut = src.clone();
     
     //read the original image
     originalImage = imread(file_name+".tif");
     
     // Ensure the image loaded
-    if( !src.data){ fprintf(stderr, "Unable to load the image. Verify the path and try again.\nDo not include the extension\n"); return 1; }
+    if( !src.data){ fprintf(stderr, "Unable to load the image. Do not include the extension\n"); return 1; }
     
     // Threshold the image
     gray_scale = threshold(src); 
-    
-    // Erode the image
-    src = smooth_input(gray_scale, count, size, type);
-    if (debug&&DEBUG) { imshow("After Erosion", src); waitKey(0); }
     
     // Fill in regions; black if too small, grey otherwise
     src = region_filling(src, &area, range);
@@ -88,28 +82,6 @@ Mat threshold(Mat src) {
     inRange(src, Scalar(0,125,0), Scalar(0,255,0), result);
     
     return result; 
-}
-/*
- Erodes and dilates the passed image multiple times determined by the value
- passed in count. The size determines the size of the pixel used
- to morph the original image. Type determines the type of pixel used.
- It expects MORPH_RECT, MORPH_CROSS, or MORPH_ELLIPSE for the type.
- */
-Mat smooth_input(Mat src, int count, int size, int type){
-    
-    Mat img;
-    
-    // Set the morph values for erosion
-    Mat morph_values = getStructuringElement(type, Size(size, size));
-    img = src.clone();
-    
-    // Morph the image
-    for (int x = 0; x < count; x++){
-        erode(img, img, morph_values);
-        dilate(img, img, morph_values);
-    }
-    
-    return img;
 }
 
 /*
@@ -145,8 +117,21 @@ Mat region_filling(Mat image, int *size, int range) {
     return filled;
 }
 
-int check_args(int *size, int *type, int *count, int *range, std::string *file_name,
-               std::string *output_name, int argc, char** argv){
+///perfroming simple image arithmatic
+//this function will superimpose two images
+void merge(const Mat &originalImage, Mat &TexCamOut, Mat &HazMap, string output_name){
+    
+    //allocate the result image
+    HazMap.create(originalImage.size(), originalImage.type());
+    //superimpose
+    addWeighted(originalImage, 0.7, TexCamOut, 0.9, 0.0,HazMap);
+    
+    //save the superimposed image
+    imwrite(output_name+".bmp", HazMap);
+}
+
+int check_args(int *range, std::string *file_name, std::string *output_name, 
+    int argc, char** argv){
     
     char option;
     int temp;
@@ -158,47 +143,10 @@ int check_args(int *size, int *type, int *count, int *range, std::string *file_n
         return 0;
     }
     
-    while ((option = getopt(argc, argv, "hus:t:c:i:o:dr:"))!= -1) {
+    while ((option = getopt(argc, argv, "hi:o:dr:"))!= -1) {
         switch(option) {
-                
-                // Size of pixel morphing image
-            case 's':
-                temp = atoi(optarg);
-                
-                if (temp < 1) {
-                    fprintf(stderr, "Invalid size passed. Must be larger than 1.\n");
-                    return 1;
-                }
-                
-                *size = temp;
-                break;
-                
-                // Type of pixel for morphing image
-            case 't':
-                temp = atoi(optarg);
-                
-                if (temp == MORPH_RECT || temp == MORPH_ELLIPSE || temp == MORPH_CROSS)
-                    *type = temp;
-                else {
-                    fprintf(stderr, "Invalid type passed. Try again.\n");
-                    return 1;
-                }
-                
-                break;
-                
-                // Number of erosions to execute
-            case 'c':
-                temp = atoi(optarg);
-                
-                if (temp < 1){
-                    fprintf(stderr, "Invalid count passed. Needs to be greater than 1.\n");
-                    return 1;
-                }
-                
-                *count = temp;
-                break;
-                
-                // Number of erosions to execute
+             
+            // Number of erosions to execute
             case 'r':
                 temp = atoi(optarg);
                 
@@ -209,12 +157,12 @@ int check_args(int *size, int *type, int *count, int *range, std::string *file_n
                 *range = temp;
                 break;
                 
-                // Input/output filenames
+            // Input/output filenames
             case 'i':
                 *file_name = std::string(optarg);
                 break;
                 
-                // Debug enabled
+            // Debug enabled
             case 'd':
                 debug = 1;
                 break;
@@ -222,7 +170,7 @@ int check_args(int *size, int *type, int *count, int *range, std::string *file_n
             case 'o':
                 *output_name = std::string(optarg);
                 break;
-                // USage or help requested
+            // Usage or help requested
             case 'u':
             case 'h':
                 usage(argv[0]);
@@ -238,28 +186,9 @@ int check_args(int *size, int *type, int *count, int *range, std::string *file_n
     return 0;
 }
 
-///perfroming simple image arithmatic
-//this function will superimpose two images
-void merge(const Mat &originalImage, Mat &TexCamOut, Mat &HazMap, string output_name){
-    
-    //allocate the result image
-    HazMap.create(originalImage.size(), originalImage.type());
-    //superimpose
-    addWeighted(originalImage, 0.7, TexCamOut, 0.9, 0.0,HazMap);
-    
-   
-    
-    //save the superimposed image
-    imwrite(output_name+".bmp", HazMap);
-}
-
 void usage(char* binary)  {
-    printf("usage: %s -s # -c # -t # -i filename -o filename\n", binary);
-    printf("\t-s Kernel size used during erosion, defaults to 5\n");
-    printf("\t-c Number of erosions to run, defaults to 5\n");
-    printf("\t-t Kernel type to use, defaults to MORPH_ELLIPSE\n");
-    printf("\t\t MORPH_RECT = 0, MORPH_CROSS = 1, MORPH_ELLIPSE = 2\n");
-    printf("\t-i Input image filename\n");
+    printf("usage: %s -r # -i filename -o filename\n", binary);
+    printf("\t-i Input image filename - Needs to be a ppm format image. \n\t\tDo not include the extension\n");
     printf("\t-o Output image filename\n");
     printf("\t-r Radius of desired sample locations, defaults to 10\n");
 }
