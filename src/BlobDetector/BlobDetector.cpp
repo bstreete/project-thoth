fba4771c1e5b5a8311ba9236559d402a1ea38528
Brayden Streeter <bstreete@email.arizona.edu>
2015-11-09 14:13:59 -0700

    Changed the blob detector to allow specifying individual file names for input, output, and original image. Ran into issues trying to run tests where images were stored in alternate directories.

--- src/BlobDetector/BlobDetector.cpp
+++ src/BlobDetector/BlobDetector.cpp
@@ -22,52 +22,55 @@ int calcArea(int radius);
 Mat threshold(Mat src);
 void merge(const Mat &image, Mat &image2, Mat &result, string output_name);
 int check_args(int *range, std::string *file_name, std::string *output_name, 
-    int argc, char** argv);
+    std::string *original_image, int argc, char** argv);
 void usage(char* binary);
-
 
 /** @function main */
 int main( int argc, char** argv ){
-    std::string file_name, output_name;
+    std::string overlay_name, output_name, original_name;
     int status, range = 10, area = 0;
-    Mat src, originalImage, TexCamOut, gray_scale, output, HazMap;
-    std::vector<std::vector<Point>> contours;
+    Mat src, original, overlay, gray_scale, output_image;
     
     // Check the passed arguments
-    status = check_args(&range, &file_name, &output_name, argc, argv);
+    status = check_args(&range, &overlay_name, &output_name, &original_name, argc, argv);
     // Usage or help requested
     if (status == -1) return 0;
     // Invalid arguments passed
     if (status == 1) return 1;
     
     // Read the textureCam output image and save a copy for the super imposition
-    //file_name = file_name+".ppm";
-    src = imread(file_name+".ppm");
-    TexCamOut = src.clone();
+    src = imread(overlay_name);
     
+    original = imread(original_name); 
     //read the original image
-    originalImage = imread(file_name+".tif");
     
-    // Ensure the image loaded
-    if( !src.data){ fprintf(stderr, "Unable to load the image. Do not include the extension\n"); return 1; }
+    // Ensure the images loaded
+    if (!src.data){ fprintf(stderr, "Unable to load the overlay.\n"); return 1; }
+    if (!original.data) { fprintf(stderr, "Unable to load the original image.\n"); return 1;}
+
+    overlay = src.clone();
     
+    if (DEBUG&&debug) imshow("Original Overlay", src);
+    if (DEBUG&&debug) imshow("Original Image", original);
+
     // Threshold the image
     gray_scale = threshold(src); 
-    
+    if (DEBUG&&debug) imshow("Threshold Result" , gray_scale); 
+
     // Fill in regions; black if too small, grey otherwise
-    src = region_filling(src, &area, range);
-    if (debug&&DEBUG) { imshow( "Result", src); waitKey(0); }
+    gray_scale = region_filling(gray_scale, &area, range);
+    if (DEBUG&&debug) { imshow( "Result", gray_scale); waitKey(0); }
 
     printf("Total Usable Area: %.02f%%\n", (double) area * 100.0/ (src.rows * src.cols));
     
-    merge(originalImage, TexCamOut, HazMap, output_name);
-    namedWindow("HazardMap");
-    
-    imshow("HazardMap", HazMap);
-    waitKey(0);
-    cout<<" output_name = "<< output_name;
+    merge(original, overlay, output_image, output_name);
+    imshow("HazardMap", output_image);
+
+
     // Save the final image for further comparisons
-    imwrite(output_name+".bmp", HazMap);
+    // imwrite(output_name, output_image);
+
+    waitKey(0);
     return 0;
 }
 
@@ -122,16 +125,16 @@ Mat region_filling(Mat image, int *size, int range) {
 void merge(const Mat &originalImage, Mat &TexCamOut, Mat &HazMap, string output_name){
     
     //allocate the result image
-    HazMap.create(originalImage.size(), originalImage.type());
+    HazMap = originalImage.clone();
     //superimpose
     addWeighted(originalImage, 0.7, TexCamOut, 0.9, 0.0,HazMap);
     
     //save the superimposed image
-    imwrite(output_name+".bmp", HazMap);
+    imwrite(output_name, HazMap);
 }
 
 int check_args(int *range, std::string *file_name, std::string *output_name, 
-    int argc, char** argv){
+    std::string *original_image, int argc, char** argv){
     
     char option;
     int temp;
@@ -142,8 +145,8 @@ int check_args(int *range, std::string *file_name, std::string *output_name,
         usage(argv[0]);
         return 0;
     }
-    
-    while ((option = getopt(argc, argv, "hi:o:dr:"))!= -1) {
+        
+    while ((option = getopt(argc, argv, "hi:f:o:dr:"))!= -1) {
         switch(option) {
              
             // Number of erosions to execute
@@ -161,15 +164,22 @@ int check_args(int *range, std::string *file_name, std::string *output_name,
             case 'i':
                 *file_name = std::string(optarg);
                 break;
-                
+            
+            // Final output filename
+            case 'f': 
+                *output_name = std::string(optarg);
+                break; 
+
+            // Original image before texture cam 
+            case 'o':
+                *original_image = std::string(optarg);
+                break;
+            
             // Debug enabled
             case 'd':
                 debug = 1;
                 break;
                 
-            case 'o':
-                *output_name = std::string(optarg);
-                break;
             // Usage or help requested
             case 'u':
             case 'h':
@@ -188,7 +198,8 @@ int check_args(int *range, std::string *file_name, std::string *output_name,
 
 void usage(char* binary)  {
     printf("usage: %s -r # -i filename -o filename\n", binary);
-    printf("\t-i Input image filename - Needs to be a ppm format image. \n\t\tDo not include the extension\n");
-    printf("\t-o Output image filename\n");
+    printf("\t-i Input overlay filename - Needs to be a ppm format image. \n\t\tDo not include the extension\n");
+    printf("\t-f Final output filename\n");
+    printf("\t-o Original Image - Prior to processing by Texture Cam\n"); 
     printf("\t-r Radius of desired sample locations, defaults to 10\n");
 }
